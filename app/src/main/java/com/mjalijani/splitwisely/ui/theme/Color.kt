deaf4464d44f17539a945da34100591fa2625f11
Mj <mj.alijani@gmail.com>
2024-09-11 09:45:21 +0330

    added colors

--- app/src/main/java/com/mjalijani/splitwisely/ui/theme/Color.kt
+++ app/src/main/java/com/mjalijani/splitwisely/ui/theme/Color.kt
@@ -5,6 +5,9 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val SecondaryText = Color(0xFF5C5C5C)
+val Primary = Color(0xFF078BDE)
+val Surface = Color(0xFFE7E7E7)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
