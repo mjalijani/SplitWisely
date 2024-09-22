3da353575cd53179bb45ca42dbf6d90271a0049f
Mj <mj.alijani@gmail.com>
2024-09-22 11:39:55 +0330

    updated

--- app/src/main/java/com/mjalijani/splitwisely/ui/account/profile/ProfileViewModel.kt
+++ app/src/main/java/com/mjalijani/splitwisely/ui/account/profile/ProfileViewModel.kt
@@ -1,11 +1,16 @@
 package com.mjalijani.splitwisely.ui.account.profile
 
 import androidx.lifecycle.ViewModel
+import com.mjalijani.splitwisely.domain.usecase.user.GetCurrentUserUseCase
+import com.mjalijani.splitwisely.domain.usecase.user.UpdateUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : ViewModel() {
+class ProfileViewModel @Inject constructor(
+    private val getCurrentUserUseCase: GetCurrentUserUseCase,
+    private val updateUserUseCase: UpdateUserUseCase
+) : ViewModel() {
 
     init {
 
