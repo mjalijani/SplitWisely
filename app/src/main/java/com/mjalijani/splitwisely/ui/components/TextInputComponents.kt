7cef9b21b04df038a20b9ee30daf89769d8e5167
mj <mj.alijani@gmail.com>
2024-10-23 15:13:07 +0330

    added OutLineTextFieldApp

--- app/src/main/java/com/mjalijani/splitwisely/ui/components/TextInputComponents.kt
+++ app/src/main/java/com/mjalijani/splitwisely/ui/components/TextInputComponents.kt
@@ -1,21 +1,33 @@
 package com.mjalijani.splitwisely.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mjalijani.splitwisely.R
 import com.mjalijani.splitwisely.ui.theme.Surface
 
 @Composable
-fun AppTextInput(
+fun TextInputApp(
     modifier: Modifier = Modifier,
     textValue: String = "https://www.spliter.com/join/p12fsdJHRsb+3164154153",
     onValueChange: (String) -> Unit
@@ -34,10 +46,71 @@ fun AppTextInput(
     )
 }
 
+@Composable
+fun OutLineTextFieldApp(
+    textValue: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier
+) {
+    val text by remember { mutableStateOf(textValue) }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { onValueChange.invoke(it) },
+        label = { Text(label) },
+        modifier = modifier
+            .fillMaxWidth()
+    )
+}
+
+@Composable
+fun PasswordOutLineTextFieldApp(
+    textValue: String,
+    onValueChange: (String) -> Unit,
+    onPassVisible: Boolean,
+    modifier: Modifier = Modifier
+) {
+    val text by remember { mutableStateOf(textValue) }
+    var passwordVisible by remember { mutableStateOf(onPassVisible) }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { onValueChange.invoke(it) },
+        label = { Text("Password") },
+        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val image = if (passwordVisible) R.drawable.ic_eye else R.drawable.ic_eye
+
+            IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                Icon(
+                    painter = painterResource(image),
+                    contentDescription = if (passwordVisible) "Hide password" else "Show password"
+                )
+            }
+        },
+        modifier = modifier
+            .fillMaxWidth()
+    )
+}
+
+@Preview
+@Composable
+private fun PasswordOutLineTextFieldAppPreview() {
+    PasswordOutLineTextFieldApp("", {}, true)
+}
+
+@Preview
+@Composable
+private fun OutLineTextFieldAppPreview() {
+    OutLineTextFieldApp("", {}, "label")
+}
+
 @Preview
 @Composable
 fun AppTextInputPreview(modifier: Modifier = Modifier) {
-    AppTextInput() {
+    TextInputApp() {
 
     }
 }
