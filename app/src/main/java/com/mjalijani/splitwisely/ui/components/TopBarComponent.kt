77be43fedd94d1b1309ac211223f21c2ff319ccb
mj <mj.alijani@gmail.com>
2024-10-28 14:19:52 +0330

    added TopActionBar

--- app/src/main/java/com/mjalijani/splitwisely/ui/components/TopBarComponent.kt
+++ app/src/main/java/com/mjalijani/splitwisely/ui/components/TopBarComponent.kt
@@ -1,11 +1,15 @@
 package com.mjalijani.splitwisely.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +19,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.mjalijani.splitwisely.R
 
 @Composable
@@ -32,6 +37,89 @@ fun TopBar(modifier: Modifier = Modifier, hasBack: Boolean = false) {
         Spacer(Modifier.size(16.dp))
         Text(stringResource(R.string.app_name), color = Color.White)
     }
+}
+
+
+@Composable
+fun TopActionBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    leftIcon: Int? = null,
+    rightIcon: Int? = null,
+    leftIconOnClick: (() -> Unit)? = null,
+    rightIconOnClick: (() -> Unit)? = null
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(0.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 32.dp
+        )
+    ) {
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+            val (back, titleText, search) = createRefs()
+
+            leftIcon?.let { icon ->
+                Image(
+                    modifier = Modifier
+                        .clickable {
+                            leftIconOnClick?.invoke()
+                        }
+                        .constrainAs(back) {
+                            start.linkTo(parent.start)
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                        },
+                    painter = painterResource(icon),
+                    contentDescription = null
+                )
+            }
+
+            TextApp(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .constrainAs(titleText) {
+                        start.linkTo(if (leftIcon != null) back.end else parent.start)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    },
+                text = title,
+                textColor = Color.Black
+            )
+
+            rightIcon?.let { icon ->
+                Image(
+                    modifier = Modifier
+                        .clickable {
+                            rightIconOnClick?.invoke()
+                        }
+                        .constrainAs(search) {
+                            end.linkTo(parent.end)
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                        },
+                    painter = painterResource(icon),
+                    contentDescription = null
+                )
+            }
+
+        }
+    }
+
+}
+
+@Preview
+@Composable
+private fun TopActionBarPreview() {
+    TopActionBar(title = stringResource(R.string.currency))
 }
 
 @Preview
