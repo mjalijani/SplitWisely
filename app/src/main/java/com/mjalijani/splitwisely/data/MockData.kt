6335bbd7bb4c28b30b88fb401ab5592b2cb3a816
mj <mj.alijani@gmail.com>
2024-11-02 14:26:31 +0330

    added profile to MockData

--- app/src/main/java/com/mjalijani/splitwisely/data/MockData.kt
+++ app/src/main/java/com/mjalijani/splitwisely/data/MockData.kt
@@ -2,6 +2,7 @@ package com.mjalijani.splitwisely.data
 
 import com.mjalijani.splitwisely.R
 import com.mjalijani.splitwisely.data.common.TransactionItem
+import com.mjalijani.splitwisely.ui.account.profile.Profile
 
 object MockData {
     val fakeTransaction = TransactionItem(
@@ -9,5 +10,12 @@ object MockData {
         title = "this is test",
         subtitle = "GameNet",
         secondTitle = "Jul 30, 9:37 PM",
+    )
+
+    val profile = Profile(
+    R.drawable.ali,
+    name = "Amir Hussein",
+    email = "aj.alijani@gmail.com",
+    phone = "+98123456789"
     )
 }
