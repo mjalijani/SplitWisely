d121877b41b40c45227178c54c1bef15027a6b00
Mohammad javad <mj.alijani@gmail.com>
2024-12-12 00:55:37 +0330

    refactor CommentScreen

--- app/src/main/java/com/mjalijani/splitwisely/ui/expense/comment/CommentScreen.kt
+++ app/src/main/java/com/mjalijani/splitwisely/ui/expense/comment/CommentScreen.kt
@@ -1,12 +1,36 @@
 package com.mjalijani.splitwisely.ui.expense.comment
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun CommentScreen(modifier: Modifier = Modifier) {
+    ConstraintLayout(
+        modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        val (sendMessage) = createRefs()
 
+
+
+        SendMessageView(modifier = Modifier
+            .padding(8.dp)
+            .constrainAs(sendMessage) {
+                bottom.linkTo(parent.bottom)
+            },
+            textValue = "",
+            send = {},
+            onValueChange = {}
+        )
+    }
 }
 
 @Preview
