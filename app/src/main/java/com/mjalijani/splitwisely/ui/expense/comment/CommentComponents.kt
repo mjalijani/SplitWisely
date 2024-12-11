40f7501a1aef580713f30464473d807aab4feddc
Mohammad javad <mj.alijani@gmail.com>
2024-12-12 00:55:23 +0330

    refactor CommentComponents

--- app/src/main/java/com/mjalijani/splitwisely/ui/expense/comment/CommentComponents.kt
+++ app/src/main/java/com/mjalijani/splitwisely/ui/expense/comment/CommentComponents.kt
@@ -1,21 +1,36 @@
 package com.mjalijani.splitwisely.ui.expense.comment
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mjalijani.splitwisely.R
+import com.mjalijani.splitwisely.helper.StringCallBack
 import com.mjalijani.splitwisely.ui.components.TextInputApp
+import com.mjalijani.splitwisely.ui.theme.Primary
+import com.mjalijani.splitwisely.ui.theme.PrimaryText
 import com.mjalijani.splitwisely.ui.theme.Surface
+import com.mjalijani.splitwisely.ui.theme.ThirdText
 
 @Composable
-fun SendMessageView(modifier: Modifier = Modifier) {
+fun SendMessageView(
+    modifier: Modifier = Modifier,
+    textValue: String = "",
+    send: StringCallBack,
+    onValueChange: StringCallBack
+) {
     Row(
         modifier = modifier
             .background(
@@ -24,8 +39,26 @@ fun SendMessageView(modifier: Modifier = Modifier) {
             )
             .clip(RoundedCornerShape(corner = CornerSize(50.dp)))
     ) {
-        TextInputApp(placeHolder = stringResource(R.string.comment)) {
+        val value by remember {
+            mutableStateOf(textValue)
+        }
 
+        TextInputApp(
+            textValue = value,
+            textColor = PrimaryText,
+            placeHolderColor = ThirdText,
+            placeHolder = stringResource(R.string.comment),
+            trailingIcon = {
+                Icon(
+                    modifier = Modifier.clickable {
+                        send.invoke(value)
+                    },
+                    painter = painterResource(R.drawable.ic_send),
+                    tint = Primary,
+                    contentDescription = null
+                )
+            }) {
+            onValueChange.invoke(it)
         }
     }
 
@@ -34,5 +67,5 @@ fun SendMessageView(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun SendMessagePreview() {
-    SendMessageView()
+    SendMessageView(textValue = "", send = {}, onValueChange = {})
 }
