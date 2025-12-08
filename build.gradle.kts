379f60a3ab1602c0387eb11e83934514b0f030e2
Mohammad <mj.alijani@gmail.com>
2025-12-08 18:01:20 +0330

    feat: Add Kotlin Compose plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.org.jetbrains.kotlin.kapt) apply false
     alias(libs.plugins.com.dagger.hilt) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
