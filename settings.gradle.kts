9486ffdfc04494134919b550e847759bfa385809
Mohammad javad <mj.alijani@gmail.com>
2025-12-26 01:24:57 +0330

    set Myket maven

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,16 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
+        maven {
+            url  = uri("https://maven.myket.ir")
         }
-        mavenCentral()
-        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
+        maven {
+            url  = uri("https://maven.myket.ir")
+        }
     }
 }
 
